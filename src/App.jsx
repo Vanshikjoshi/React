9bd0d6f7712f04ba01df0fc2349d510100b9220a
Vanshika joshi <vanshikajoshi072@gmail.com>
2026-04-01 17:52:07 +0530

    practiced questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from "react";
-import Counter from "./UserComponent";
-function App(){
-  const [count, setCount] = useState(0);
-  const[data, setData] = useState(0);
-  const[display, setDisplay] = useState(true);
-  return(
-    <div>
-      <h1>Use Effect </h1>
-      {
-        display?<Counter count={count} data={data}></Counter>:null
-      }
-      <button onClick={()=>setCount(count+1)}>Count</button>
-      <button onClick={()=>setData(data+1)}>Data</button>
-      <button onClick={()=>setDisplay(!display)}>Toggle</button>
+
+function App() {
+  const [color, setColor] = useState("white");
+
+  return (
+    <div style={{ backgroundColor: color, height: "100vh" }}>
+      <button onClick={() => setColor("green")}>
+        Change color
+      </button>
     </div>
-  )
+  );
 }
+
 export default App;
